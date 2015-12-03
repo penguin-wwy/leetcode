ace539f1be0cc5cb2a356e57c7f56f86b72d24de
penguin-wwy <wwy950210@163.com>
2015-12-03 22:56:47 +0800

    142Linked List Cycle2
    Given a linked list, return the node where the cycle begins. If there is
    no cycle, return null.
    Note: Do not modify the linked list.

---
+++ 142Linked List Cycle2.c
@@ -0,0 +1,26 @@
+﻿/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *detectCycle(struct ListNode *head) {
+    struct ListNode *fast = head, *slow = head;
+ 	while(fast && fast->next)
+ 	{
+	 	fast = fast->next->next;
+	 	slow = slow->next;
+	 	if(fast == slow)
+	 	{
+	 		struct ListNode *slow2 = head;
+	 		while(slow != slow2)
+	 		{
+		 		slow = slow->next;
+		 		slow2 = slow2->next;
+		 	}
+		 	return slow2;
+	 	}
+	 }
+	 return NULL;
+}
