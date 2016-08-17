f74ae9c2dedae1cbce592ce65545e6a0fcfd7fff
penguin-wwy <940375606@qq.com>
2016-08-17 21:57:43 +0800

    119. Pascal's Triangle II
    Given an index k, return the kth row of the Pascal's triangle.

---
+++ 119. Pascal's Triangle II.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        numRows = rowIndex + 1
+        if numRows == 0:
+            return []
+        elif numRows == 1:
+            return [1]
+        elif numRows == 2:
+            return [1, 1]
+    
+        res = [[1], [1, 1]]
+        for col in range(3, numRows+1):
+            tmp = []
+            for i in range(0, col):
+                if i == 0 or i == col - 1:
+                    tmp.append(1)
+                else:
+                    tmp.append(res[col - 2][i - 1] + res[col - 2][i])
+            res.append(tmp)
+        return res[rowIndex]
